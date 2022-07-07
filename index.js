ba322535e310126bbbb929d1aecd4b1de8348072
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-07 23:49:16 +0000

    Implemented fetch fly over times function with my coordinates as input

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 
 // fetchMyIP((error, ip) => {
@@ -6,14 +6,23 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //     console.log("It didn't work!" , error);
 //     return;
 //   }
-
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-fetchCoordsByIP('50.93.50.22', (error, coordinates) => {
+// fetchCoordsByIP('50.93.50.22', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinates: ', coordinates);
+// });
+
+const myCoords = { latitude: '52.83307', longitude: '-110.8504696' };
+
+fetchISSFlyOverTimes(myCoords, (error, flyOverTimes) => {
   if (error) {
     console.log("It didn't work!", error);
-    return; 
+    return;
   }
-  console.log('It worked! Returned coordinates: ', coordinates)
-})
+  console.log('It worked! Returned fly over times ', flyOverTimes);
+});
